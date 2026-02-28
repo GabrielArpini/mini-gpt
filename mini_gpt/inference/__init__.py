1651c1d9ea59398dc793b40b8dc9fcf4d8b79206
GabrielArpini <arpinigabrielsr71@gmail.com>
2026-02-28 17:01:29 -0300

    chore: better overall strucutre for project

---
+++ mini_gpt/inference/__init__.py
@@ -0,0 +1,7 @@
+from mini_gpt.inference.engine import Engine
+from mini_gpt.inference.sampling import (
+    temperature_sampling,
+    top_k_filtering,
+    top_p_filtering,
+    apply_ngram_penalty,
+)
