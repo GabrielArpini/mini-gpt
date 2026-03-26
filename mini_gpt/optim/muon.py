017fb90cd4b866f77ec5769cb4da382f0579617b
GabrielArpini <arpinigabrielsr71@gmail.com>
2026-03-26 14:54:18 -0300

    Remove unnecessary assertion

--- mini_gpt/optim/muon.py
+++ mini_gpt/optim/muon.py
@@ -74,7 +74,8 @@ class Muon(torch.optim.Optimizer):
             params: list[Tensor] = group["params"]
             for p in params:
                 g = p.grad
-                assert g is not None
+                if g is None:
+                    continue
                 state = self.state[p]
                 if "momentum_buffer" not in state:
                     state["momentum_buffer"] = torch.zeros_like(g)
